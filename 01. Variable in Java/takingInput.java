a24b4ca81c2cf334d13d4fdbc3aed9e4a0a04091
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-25 20:41:37 +0530

    taking input

--- 01. Variable in Java/takingInput.java
+++ 01. Variable in Java/takingInput.java
@@ -1,3 +1,13 @@
-public class takingInput {
-    
+import java.util.*;
+public class takingInput{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // for string
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        System.out.println( "Name is : "+ name);
+        // for integer
+        System.out.println(age);
+         
+    }
 }
