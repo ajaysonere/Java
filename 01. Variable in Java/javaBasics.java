298bbcc055c84927b127d1be404303bf12914369
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-25 19:53:38 +0530

    \n

--- 01. Variable in Java/javaBasics.java
+++ 01. Variable in Java/javaBasics.java
@@ -1,5 +1,6 @@
 public class javaBasics{
     public static void main(String args[]){
-        System.out.print("Hello world");
+        System.out.print("Hello world\n");
+        System.out.println("Hello world from new line");
     }
 }
