3bdec1b214e1e4e15e06d09b019f21bb9c98e583
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-25 20:21:11 +0530

    sum of two number

--- 01. Variable in Java/sumOfTwoNumber.java
+++ 01. Variable in Java/sumOfTwoNumber.java
@@ -1,5 +1,9 @@
 public class sumOfTwoNumber{
     public static void main(String args[]){
-        
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+        int c = a+b;
+        System.out.println(c);
     }
 }
