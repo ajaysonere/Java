043b7d5c06e09bdc725811a7c1d6c50181a3c8ec
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-25 20:10:15 +0530

    java basics

--- 01. Variable in Java/javaBasics.java
+++ 01. Variable in Java/javaBasics.java
@@ -2,5 +2,11 @@ public class javaBasics{
     public static void main(String args[]){
         System.out.print("Hello world\n");
         System.out.println("Hello world from new line");
+        int a = 10;
+        int b = 20;
+        System.out.println(a);
+        System.out.println(b);
+        a = b;
+        System.out.println(a);
     }
 }
