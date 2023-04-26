337b96189351052283c9c96355cecfc5432d82e8
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-26 11:15:52 +0530

    area of circle

--- 01. Variable in Java/areaOfCircle.java
+++ 01. Variable in Java/areaOfCircle.java
@@ -1,3 +1,11 @@
-public class areaOfCircle {
-    
+import java.util.*;
+public class areaOfCircle{
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Area of Circle ");
+       float radius = sc.nextFloat();
+       float areaOfCircle = 3.14f*radius*radius;
+       System.out.println("Area is "+areaOfCircle);
+       sc.close();
+    }
 }
