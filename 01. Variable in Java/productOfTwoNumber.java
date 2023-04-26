3979fea42b96e5d22d6fc3e6710724a72fee5d5c
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-26 10:41:52 +0530

    product of two number

---
+++ 01. Variable in Java/productOfTwoNumber.java
@@ -0,0 +1,8 @@
+import javax.swing.plaf.synth.SynthStyle;
+
+public class productOfTwoNumber{
+    public static void main(String args[]){
+        System.out.println("Product Of A and B");
+        
+    }
+}
