bf16edca7970531066236d98017b79cbe8188612
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-27 22:30:17 +0530

    type conversion

--- 01. Variable in Java/typeConversion.java
+++ 01. Variable in Java/typeConversion.java
@@ -2,9 +2,30 @@ import java.util.*;
 public class typeConversion{
     public static void main(String args[]){
       Scanner sc = new Scanner(System.in);
-
-      int a = 1523;
+      int a = sc.nextInt();
       long b = a;
       System.out.println(b);
+
+      float x = sc.nextFloat();
+      int y = (int) x;
+      System.out.println(y);
+
+      char ch = '@';
+      int number = (int) ch;
+      System.out.println(number);
+
+      // type promotion
+      /*
+        1) byte , short , char will be automatically converted in integer value
+        2) long , float , doulbe will be automatically converted in max type of variable
+      */
+
+      byte number1 = 10;
+      short number2 = 152;
+      char char1 = 'a';
+      int ans = number1+number2+char1;
+      System.out.println(ans);
+
+
     }
 }
