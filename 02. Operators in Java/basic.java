91b272540fca71b84c4dd38bf34bbc466a1c09f8
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-27 22:53:51 +0530

    arithmatic operator

--- 02. Operators in Java/basic.java
+++ 02. Operators in Java/basic.java
@@ -1,5 +1,17 @@
 public class basic{
     public static void main(String args[]){
         // operators in java
+        // 1. Arithmetic operator
+            // Binary operators
+            int a = 10;
+            int b = 20;
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+            System.out.println(a%b);
+            // uninary operators
+            a++;
+            System.out.println(a);
     }
 }
