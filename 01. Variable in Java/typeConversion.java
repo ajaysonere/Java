96ab332725f1a51f918899bc6d4bce412ae871ca
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-27 21:54:14 +0530

    type conversion

---
+++ 01. Variable in Java/typeConversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class typeConversion{
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+
+      int a = 1523;
+      long b = a;
+      System.out.println(b);
+    }
+}
