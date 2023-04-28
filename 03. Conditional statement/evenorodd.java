af1f37b131273b99cd6246d43f0a9f21a3bde319
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-28 14:32:02 +0530

    even and odd in jav

--- 03. Conditional statement/evenorodd.java
+++ 03. Conditional statement/evenorodd.java
@@ -2,9 +2,12 @@ import java.util.*;
 public class evenorodd{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        if(n%2 == 0){
-            System.out.
+        int number = in.nextInt();
+        if(number%2 == 0){
+            System.out.println("Number is even");
+        }else {
+            System.out.println("Number is odd ");
         }
+        in.close();
     }
 }
