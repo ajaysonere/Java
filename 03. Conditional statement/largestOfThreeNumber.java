c78b5ad951e2bdb0a34221e8fe6311e1ee75e96d
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-29 08:51:36 +0530

    largest

--- 03. Conditional statement/largestOfThreeNumber.java
+++ 03. Conditional statement/largestOfThreeNumber.java
@@ -5,15 +5,21 @@ public class largestOfThreeNumber {
        int firstNumber = in.nextInt();
        int secondNumber = in.nextInt();
        int thirdNumber = in.nextInt();
+       int largest = 0;
        if(firstNumber > secondNumber && firstNumber > thirdNumber){
           System.out.println("The Greatest Number is First Number "+firstNumber);
+          largest = firstNumber;
        }
-       else if(secondNumber > firstNumber && secondNumber > thirdNumber){
+       else if(secondNumber > thirdNumber){
           System.out.println("The Greatest Number is Second Number "+secondNumber);
+          largest = secondNumber;
        }
        else{
           System.out.println("The Greatest Number is Third Number "+ thirdNumber);
+          largest = thirdNumber;
        }
        in.close();
+       String isEvenOrOdd = (largest%2==0 ? "Even" : "Odd" );
+       System.out.println("Number is "+isEvenOrOdd);
     }
 }
