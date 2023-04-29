1db78d71e2d3f991a75316bda94c0803a176534b
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-29 14:13:25 +0530

    switch condition

---
+++ 03. Conditional statement/SwitchConditional.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SwitchConditional {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int spin = in.nextInt();
+        switch (spin) {
+            case 1:
+                System.out.println("You get 5% discount");
+                break;
+            case 2:
+                System.out.println("You get 10% discount");
+                break;
+            case 3:
+                System.out.println("You get 20% Max discount");
+                break;
+            default:
+                System.out.println("Sorry ! better luck next time :) ");
+        }
+    }
+}
