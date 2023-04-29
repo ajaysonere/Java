b3f49c701f07a2ec271f94ba773852c6109e2f54
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-29 13:53:08 +0530

    switch

--- 03. Conditional statement/switch.java
+++ 03. Conditional statement/switch.java
@@ -1,3 +1,16 @@
-public class switch {
-    
+import java.util.*;
+public class Switch{
+    public static void main(String args[]){
+       Scanner in = new Scanner(System.in);
+       int spin = in.nextInt();
+       switch(spin){
+          case 1: System.out.println("You get 5% discount");
+                  break;
+          case 2: System.out.println("You get 10% discount");
+                  break;
+          case 3: System.out.println("You get 20% Max discount");
+                  break;
+          default : System.out.println("Sorry ! better luck next time :) ");
+       }
+    }
 }
