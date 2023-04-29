08586da24f98f2c56746d047588ee49091282538
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-29 08:36:48 +0530

    largest of 3

--- 03. Conditional statement/largestOfThreeNumber.java
+++ 03. Conditional statement/largestOfThreeNumber.java
@@ -1,3 +1,19 @@
+import java.util.*;
 public class largestOfThreeNumber {
-    
+    public static void main(String args[]){
+       Scanner in = new Scanner(System.in);
+       int firstNumber = in.nextInt();
+       int secondNumber = in.nextInt();
+       int thirdNumber = in.nextInt();
+       if(firstNumber > secondNumber && firstNumber > thirdNumber){
+          System.out.println("The Greatest Number is First Number "+firstNumber);
+       }
+       else if(secondNumber > firstNumber && secondNumber > thirdNumber){
+          System.out.println("The Greatest Number is Second Number "+secondNumber);
+       }
+       else{
+          System.out.println("The Greatest Number is Third Number "+ thirdNumber);
+       }
+       in.close();
+    }
 }
