48f2b00bd8c062c1deb0db5cdd83519b094177f4
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-29 08:57:28 +0530

    pass or fail

--- 03. Conditional statement/passOrFail.java
+++ 03. Conditional statement/passOrFail.java
@@ -1,3 +1,15 @@
+import java.util.*;
 public class passOrFail {
-    
+    public static void main(String args[]){
+       Scanner in = new Scanner(System.in);
+       float marks = in.nextFloat();
+       if(marks >33.0){
+          System.out.println("Greade : Pass");
+       }else{
+          System.out.println("Greade :Fail");
+       }
+
+       String isPass = (marks>33)?"Pass":"Fail";
+       System.out.println("Greade : "+isPass);
+    }
 }
