e3c0f0b45d30e9d1897268fa25ef38ae39e0c057
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-30 10:04:11 +0530

    number is positive or negative

--- 03. Conditional statement/positiveAndNegative.java
+++ 03. Conditional statement/positiveAndNegative.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class positiveAndNegative{
     public static void main(String args[]){
-        
+       Scanner in  = new Scanner(System.in);
+       int number = in.nextInt();
+       if(number < 0){
+          System.out.println("Number is Negative");
+       }else{
+          System.out.println("Number is Positive ");
+       }
+       in.close();
     }
 }
