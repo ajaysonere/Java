a1f4233a9b8d4b6188e2b86c207e67169e498ac4
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-30 10:29:12 +0530

    print 1 to N

--- 04. Loops/basicOfLoops.java
+++ 04. Loops/basicOfLoops.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class basicOfLoops{
     public static void main(String args[]){
        // Basic of while Loops
---
+++ 04. Loops/print1toN.java
@@ -0,0 +1,10 @@
+public class print1toN{
+    public static void main(String args[]){
+        int n = 10;
+        int count = 1;
+        while(count <= n){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
