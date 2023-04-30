fef447d62cf55a514cc846015d3cba5ee788ba2f
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-30 10:25:20 +0530

    loops

--- 04. Loops/basicOfLoops.java
+++ 04. Loops/basicOfLoops.java
@@ -1,5 +1,10 @@
 public class basicOfLoops{
     public static void main(String args[]){
-        
+       // Basic of while Loops
+       int counter = 0;
+       while(counter < 10){
+          System.out.println("Hello World " + counter);
+          counter++;
+       }
     }
 }
