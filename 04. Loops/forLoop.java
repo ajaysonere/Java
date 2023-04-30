bc3da1649d8ff94a3b39dd0157c6953ddc1879a5
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-30 17:59:56 +0530

    for loop

--- 04. Loops/forLoop.java
+++ 04. Loops/forLoop.java
@@ -1,5 +1,8 @@
 public class forLoop{
     public static void main(String args[]){
-        
+        int count;
+        for(count=1; count <= 10; count++){
+             System.out.println(count);
+        }
     }
 }
