77c4757b8bef2e0a63fc17887bef9510028a549f
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-01 14:34:23 +0530

    do while

--- 04. Loops/doWhile.java
+++ 04. Loops/doWhile.java
@@ -1,5 +1,10 @@
 public class doWhile {
     public static void main(String args[]){
         // do while loop
+        int counter = 1;
+        do{
+            System.out.println("Hey " + counter);
+            counter++;
+        }while(counter <= 10);
     }
 }
