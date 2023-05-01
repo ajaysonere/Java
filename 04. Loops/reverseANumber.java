b4e21110c3347c43263055548cb057c5075d691e
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-01 14:10:03 +0530

    reverse a number

--- 04. Loops/reverseANumber.java
+++ 04. Loops/reverseANumber.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 public class reverseANumber{
     public static void main(String args[]){
         // reverse a number
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int result = 0;
+        while(n>0){
+           int temp = n%10;
+           result = result*10+temp;
+           n = n/10;
+        }
+        System.out.println("Reverse of a number is : "+ result);
+        in.close();
     }
 }
