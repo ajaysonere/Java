7d648f4adfc63895ff0864c6fa688cdec8d3751c
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-01 14:02:01 +0530

    square pattern

--- 04. Loops/squarePattern.java
+++ 04. Loops/squarePattern.java
@@ -1,5 +1,29 @@
+import java.util.*;
+
 public class squarePattern{
     public static void main(String args[]){
         // print square pattern
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int res = n;
+
+        System.out.println("Using the for loop");
+        for(int i=0; i<n;i++){
+            for(int j=0; j<n; j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        System.out.println("Using the while loop");
+        int i =0;
+        while(i < n){
+            int j = 0;
+            while(j<n){
+                System.out.print("* ");
+                j++;
+            }
+            System.out.print("\n");
+            i++;
+        }
     }
 }
