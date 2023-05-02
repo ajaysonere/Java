a4d64bec5fb79457e28d582b9fd00c821cb215fb
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-02 10:09:27 +0530

    pattern four

--- 05. Pattern/patternFour.java
+++ 05. Pattern/patternFour.java
@@ -1,3 +1,14 @@
 public class patternFour {
-    
+    public static void main(String args[]){
+        System.out.println("Character Pattern ");
+        char ch = 'A';
+        int n = 6;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
 }
