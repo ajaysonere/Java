ecbf360c0d512bb73700a525ae972e79ae62352e
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-02 09:07:48 +0530

    pattern two

--- 05. Pattern/patternTwo.java
+++ 05. Pattern/patternTwo.java
@@ -1,7 +1,15 @@
+import java.util.*;
 public class patternTwo {
     public static void main(String args[]){
-        // patter two
+        // pattern two
+        Scanner in = new Scanner(System.in);
         System.out.println("Pattern Two");
-        
+        int pattern = in.nextInt();
+        for(int i=0 ; i<pattern; i++){
+            for(int j= pattern; j>i; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
