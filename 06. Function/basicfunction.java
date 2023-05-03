95f8974f5adc9e0fe46eba57714fa88449f83582
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-03 09:08:25 +0530

    basic function

--- 06. Function/basicfunction.java
+++ 06. Function/basicfunction.java
@@ -1,9 +1,21 @@
-
+import java.util.*;
 public class basicfunction {
+
     public static void printHelloWorld(){
         System.out.println("Hello world ");
     }
+
+    public static int addition(int a , int b){
+        return a+b;
+    }
     public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
         printHelloWorld();
+        int result = addition(a, b);
+        System.out.println("Result is :"+ result);
+        in.close();
+
     }
 }
