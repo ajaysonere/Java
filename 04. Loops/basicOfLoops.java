bcc83b06e2a16e62f9306ecabaa49cd7ad6a9399
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-03 09:04:59 +0530

    function

--- 04. Loops/basicOfLoops.java
+++ 04. Loops/basicOfLoops.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 public class basicOfLoops{
     public static void main(String args[]){
        // Basic of while Loops
---
+++ 06. Function/basicfunction.java
@@ -0,0 +1,9 @@
+
+public class basicfunction {
+    public static void printHelloWorld(){
+        System.out.println("Hello world ");
+    }
+    public static void main(String args[]){
+        printHelloWorld();
+    }
+}
