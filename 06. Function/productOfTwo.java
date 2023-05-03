cfb09c31350e3fe67d2ff328d08a672cede60277
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-03 09:45:40 +0530

    product of two number

--- 06. Function/productOfTwo.java
+++ 06. Function/productOfTwo.java
@@ -1,12 +1,16 @@
 import java.util.*;
 public class productOfTwo {
 
-    publi static 
+    public static int productOfTwoNumber(int a , int b){
+        int product  = a*b;
+        return product;
+    }
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
         int b = in.nextInt();
         int product = productOfTwoNumber(a,b);
         System.out.println("Product is " + product);
+        in.close();
     }
 }
