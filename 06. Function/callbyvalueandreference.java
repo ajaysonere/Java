58b02d24aba8dbb4a6571009f5f653f6fa13f5ff
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-03 09:38:45 +0530

    call by value

--- 06. Function/callbyvalueandreference.java
+++ 06. Function/callbyvalueandreference.java
@@ -1,5 +1,16 @@
 public class callbyvalueandreference {
+
+    public static void callByValue(int a , int b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
     public static void main(String args[]){
-        
+        System.out.println("Call by value and call by reference ");
+        int a = 10;
+        int b = 5;
+        callByValue(a,b);
+        System.out.println("Call by Value : " +  "a =  " + a + " b = " + b );
     }
 }
