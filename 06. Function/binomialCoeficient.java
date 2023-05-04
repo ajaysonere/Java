e7423d167435a25a98336fdebf3ab1242e7cd685
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-04 22:39:54 +0530

    binomail coeficient

--- 06. Function/binomialCoeficient.java
+++ 06. Function/binomialCoeficient.java
@@ -1,3 +1,25 @@
+import java.util.*;
+
 public class binomialCoeficient {
-    
+
+    public static int findFactorail(int n){
+        int ans = 1;
+        for(int i=1; i<=n; i++){
+            ans *= i;
+        }
+        return ans;
+    }
+
+    public static int binomialCoeficient(int n , int r){
+        int ans;
+        ans = (findFactorail(n)/(findFactorail(r)*(findFactorail(n-r))));
+        return ans;
+    }
+    public static void main(String args[]){
+       Scanner in = new Scanner(System.in);
+       int n = in.nextInt();
+       int r = in.nextInt();
+       System.out.println("Binomia Coeficient : "+ binomialCoeficient(n,r));
+       in.close();
+    }
 }
