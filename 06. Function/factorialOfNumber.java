fdaf66fb5d7395141b52ee6c0ddbe3e381fd2be7
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-04 22:17:03 +0530

    find factorial

--- 06. Function/factorialOfNumber.java
+++ 06. Function/factorialOfNumber.java
@@ -1,5 +1,20 @@
+import java.util.*;
 public class factorialOfNumber {
+
+    public static int findFactorail(int num){
+        int ans  = 1;
+        while(num != 0){
+            ans *= num;
+            num--;
+        }
+        return ans;
+    }
     public static void main(String args[]){
         // factorial
+        Scanner in = new Scanner(System.in);
+        int nubmer = in.nextInt();
+        int factorial = findFactorail(nubmer);
+        System.out.println("Factorial Of Number is : "+ factorial);
+
     }
 }
