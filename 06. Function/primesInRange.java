140e9e3cd8412f03bfc29c2a46ceb39ca71b689f
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-05 09:45:38 +0530

    primes in range

--- 06. Function/primesInRange.java
+++ 06. Function/primesInRange.java
@@ -1,5 +1,28 @@
+import java.util.*;
 public class primesInRange {
+
+    public static boolean isPrime(int n){
+        for(int i=2; i<= Math.sqrt(n); i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void primeInRange(int n){
+        for(int i=2; i<=n; i++){
+            if(isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println();
+    }
     public static void main(String args[]){
         // Primes in range
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        primeInRange(n);
+        in.close();
     }
 }
