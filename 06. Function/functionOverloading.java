38f93aef9e7e03c41dd6a6446e15c6c250f33aae
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-05 09:15:09 +0530

    function overloading using parameter

--- 06. Function/functionOverloading.java
+++ 06. Function/functionOverloading.java
@@ -1,6 +1,23 @@
+import java.util.*;
 public class functionOverloading {
+
+    public static int sum(int a , int b){
+        return a+b;
+    }
+
+    // function overloading by parameter
+
+    public static int sum(int a, int b ,int c){
+        return a+b+c;
+    }
     public static void main(String args[]){
         // function overLoading
-        
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        System.out.println("Sum :) "+ sum(a , b));
+        System.out.println("Sum :) "+ sum(a, b, c));
+        in.close();
     }
 }
