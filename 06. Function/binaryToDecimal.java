5616d258f7d58b2bc2a490910619890294227da8
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-10 21:48:39 +0530

    binary to decimal

--- 06. Function/binaryToDecimal.java
+++ 06. Function/binaryToDecimal.java
@@ -1,5 +1,15 @@
 public class binaryToDecimal {
     public static void main(String args[]){
         // binary to decimal
+        int a = 101;
+        int pow = 0;
+        int ans = 0;
+        while(a>0){
+          int lastDigit = a%10;
+          ans = ans + (lastDigit*(int)Math.pow(2, pow));
+          pow++;
+          a = a/10;
+        }
+        System.out.println("Decimal to binary "+ ans);
     }
 }
--- 06. Function/decimalToBinary.java
+++ 06. Function/decimalToBinary.java
@@ -1,5 +1,12 @@
 public class decimalToBinary {
     public static void main(String args[]){
         // decimal to binary
+        int d = 5;
+        int ans = 0;
+        while(d>0){
+            int temp = d%2;
+            ans = ans*10+temp;
+            d = d/2;
+        }
     }
 }
