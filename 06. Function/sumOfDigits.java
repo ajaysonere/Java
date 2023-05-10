44c9d9fc017e534016d21b17b69f7ec317a263fb
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-10 22:23:34 +0530

    digit sum

--- 06. Function/sumOfDigits.java
+++ 06. Function/sumOfDigits.java
@@ -1,3 +1,11 @@
 public class sumOfDigits {
-    
+    public static void main(String args[]){
+        int n = 452;
+        int ans = 0;
+        while(n>0){
+            ans = ans + n%10;
+            n = n/10; 
+        }
+        System.out.println("Sum of Digit is : " + ans);
+    }
 }
