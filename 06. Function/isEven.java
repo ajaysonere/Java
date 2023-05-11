86f12f096e6b1a3b06320ac7089cafc52386ada5
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-11 22:00:33 +0530

    is palindrom

--- 06. Function/isEven.java
+++ 06. Function/isEven.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class isEven {
 
-    public static boolean isEven(int number){
+    public static boolean iseven(int number){
         return number%2==0;
     }
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();
-        if(isEven(number)){
+        if(iseven(number)){
             System.out.println("Number is Even ");
         }else{
             System.out.println("Number is Odd ");
--- 06. Function/isPalindrom.java
+++ 06. Function/isPalindrom.java
@@ -1,3 +1,25 @@
+import java.util.*;
 public class isPalindrom {
-    
+
+    public static boolean isPal(int number){
+        int check = number;
+        int ans = 0;
+        while(number > 0){
+            int temp = number%10;
+            ans = ans*10+temp;
+            number = number/10;
+        }
+        return (check == ans);
+    }
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        boolean isPalindrom = isPal(number);
+        if(isPalindrom){
+            System.out.println("Number is Palindrom ");
+        }else{
+            System.out.println("Number is not Palindrom ");
+        }
+        in.close();
+    }
 }
