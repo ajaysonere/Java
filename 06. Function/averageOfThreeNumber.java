3f8811ba85b565d8a3570c424ef825772c8a75e0
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-11 21:34:22 +0530

    average of three number

--- 06. Function/averageOfThreeNumber.java
+++ 06. Function/averageOfThreeNumber.java
@@ -1,3 +1,12 @@
+import java.util.*;
 public class averageOfThreeNumber {
-    
+    public static void main(String args[]){
+        Scanner in  = new Scanner(System.in);
+        int firstNumber = in.nextInt();
+        int secondNumber = in.nextInt();
+        int thirdNumber = in.nextInt();
+        int ans = (firstNumber+secondNumber+thirdNumber)/3;
+        System.out.println("Average of Three Number :"+ ans);
+        in.close();
+    }
 }
