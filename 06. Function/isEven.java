7e175a62cfb3db9050f43e6a98060ddbc047b258
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-11 21:44:25 +0530

    is even

--- 06. Function/isEven.java
+++ 06. Function/isEven.java
@@ -1,3 +1,17 @@
+import java.util.*;
+
 public class isEven {
-    
+
+    public static boolean isEven(int number){
+        return number%2==0;
+    }
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        if(isEven(number)){
+            System.out.println("Number is Even ");
+        }else{
+            System.out.println("Number is Odd ");
+        }
+    }
 }
