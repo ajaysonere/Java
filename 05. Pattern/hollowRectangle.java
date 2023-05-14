d5e3de21145dba092375f2c2ec5a36ae19f40486
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-14 08:47:57 +0530

    hollow ractanle opt

--- 05. Pattern/hollowRectangle.java
+++ 05. Pattern/hollowRectangle.java
@@ -7,10 +7,15 @@ public class hollowRectangle {
         int n = in.nextInt();
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(i == 0 || i == n-1){
-                    System.out.print("* ");
-                }
-                else if(j == 0 || j == n-1){
+                // if(i == 0 || i == n-1){
+                //     System.out.print("* ");
+                // }
+                // else if(j == 0 || j == n-1){
+                //     System.out.print("* ");
+                // }else{
+                //     System.out.print("  ");
+                // }
+                if(i==0 || i== n-1 || j==0 || j==n-1){
                     System.out.print("* ");
                 }else{
                     System.out.print("  ");
