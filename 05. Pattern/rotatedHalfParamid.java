8dc61eb011bfdeab1d5644e81ecafb4b5fee3b22
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-14 09:40:42 +0530

    rotate half paramid

--- 05. Pattern/rotatedHalfParamid.java
+++ 05. Pattern/rotatedHalfParamid.java
@@ -1,5 +1,19 @@
+import java.util.*;
 public class rotatedHalfParamid {
     public static void main(String args[]){
         // rotated half paramid
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(n-j >= i){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+        in.close();
     }
 }
