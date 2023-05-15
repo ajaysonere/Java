6d6dbc87c2f5e4dfcbf445e62ab27d4ad2c7c484
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-15 18:08:35 +0530

    zero one pattern

--- 05. Pattern/zeroOnePattern.java
+++ 05. Pattern/zeroOnePattern.java
@@ -3,7 +3,6 @@ public class zeroOnePattern {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int counter = 1;
         for(int i=0; i<n; i++){
             for(int j = 0; j<=i; j++){
                 if((i+j)%2 == 0)
