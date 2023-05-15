bcd69b9cd357397fd0eb7b408223c3502ba6ab4c
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-15 17:06:06 +0530

    invert half paramid of number

--- 05. Pattern/inverthalfparamidnumber.java
+++ 05. Pattern/inverthalfparamidnumber.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class inverthalfparamidnumber {
-    
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=1; j<= n-i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
 }
