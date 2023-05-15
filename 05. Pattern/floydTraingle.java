309c9c519b422c7be50f09124451f731c29728cf
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-15 17:20:37 +0530

    floyd traingle

--- 05. Pattern/floydTraingle.java
+++ 05. Pattern/floydTraingle.java
@@ -1,3 +1,15 @@
+import java.util.*;
 public class floydTraingle {
-    
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int index = 1;
+        for(int i=0; i<n; i++){
+            for(int j = 0; j<=i; j++){
+                System.out.print(index++ + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
 }
