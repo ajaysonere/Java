12f53a27257c3e3a39c7c6b37d988ad83394f35e
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-17 19:33:45 +0530

    butterfly

--- 05. Pattern/butterflyPattern.java
+++ 05. Pattern/butterflyPattern.java
@@ -1,3 +1,36 @@
+import java.util.*;
+
 public class butterflyPattern {
-    
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=1 ; i<=n*2-1; i++){
+            if(i<=n){
+                int space = n*2-i*2;
+                for(int j=1 ; j<=i; j++){
+                    System.out.print("*");
+                }
+                for(int j=0; j<space; j++){
+                    System.out.print(" ");
+                }
+                for(int j=1; j<=i ; j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }else{
+                int space = i*2-n*2;
+                for(int j=0; j<n-(i-n); j++){
+                    System.out.print("*");
+                }
+                for(int j=0; j<space; j++){
+                    System.out.print(" ");
+                }
+                for(int j=0; j<n-(i-n); j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        in.close();
+    }
 }
