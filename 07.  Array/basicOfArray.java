acdcf87309391f266424a836a12052ad7f6651a6
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-23 22:12:48 +0530

    linear search

--- 07.  Array/basicOfArray.java
+++ 07.  Array/basicOfArray.java
@@ -1,3 +1,8 @@
+import java.util.*;
 public class basicOfArray {
-    
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        int arr[] = new int[size]; 
+    }
 }
