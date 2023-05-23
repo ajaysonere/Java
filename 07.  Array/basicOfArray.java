78f9bf53c6d0d7dce0e9790d97bd123f8aa65aba
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-23 22:17:43 +0530

    basic operation on array

--- 07.  Array/basicOfArray.java
+++ 07.  Array/basicOfArray.java
@@ -3,6 +3,21 @@ public class basicOfArray {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int size = in.nextInt();
+        // creation of array
         int arr[] = new int[size]; 
+        // input in array
+        for(int i=0; i<size; i++){
+            arr[i] = in.nextInt();
+        }
+        // output in array
+        for(int i=0; i<size; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+        // update an array
+        for(int i=0; i<size; i++){
+            arr[i] *= 2;
+            System.out.print(arr[i]+" ");
+        }
     }
 }
