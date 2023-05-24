1634dadd85442a88bd5aacb9fab425ee2131c4dc
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-24 06:45:06 +0530

    binary search

--- 07.  Array/binarySearch.java
+++ 07.  Array/binarySearch.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
+
 public class binarySearch {
-    
+
+    public static int binarySearch(int arr[] , int key){
+        int start = 0;
+        int end = arr.length;
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(arr[mid] == key){
+                return mid;
+            }else if(arr[mid]<key){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt(); 
+        int arr[] = new int[size];
+        for(int i=0; i<size; i++){
+            arr[i] = in.nextInt();
+        }
+        int key = in.nextInt();
+        int result = binarySearch(arr,key);
+        if(result == -1){
+            System.out.println("Element is not present ");
+        }else{
+            System.out.println("Element is present at index "+ result);
+        }
+        in.close();
+    }
 }
