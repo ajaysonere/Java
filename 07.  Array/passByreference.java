2624111ceacdc4b65908d902aeed65f83b11fd47
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-24 06:26:27 +0530

    pass by reference

--- 07.  Array/passByreference.java
+++ 07.  Array/passByreference.java
@@ -1,5 +1,32 @@
+import java.util.*;
+
+
 public class passByreference {
+    public static void updateArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            arr[i] *= 2;
+        }
+    }
     public static void main(String args[]){
-        
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        // creating array
+        int arr[] = new int[size];
+        // taking input in array
+        for(int i=0; i<size; i++){
+            arr[i] = in.nextInt();
+        }
+        // array is 
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+        updateArray(arr);
+        // updated array
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        in.close();
     }
 }
