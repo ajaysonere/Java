66e13f4890781bede280ed3d9198d58eecece378
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-27 18:06:42 +0530

    print sub array

--- 07.  Array/printSubAray.java
+++ 07.  Array/printSubAray.java
@@ -1,3 +1,23 @@
+import java.util.*;
+
 public class printSubAray {
-    
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        int arr[] = new int[size];
+        //taking input
+        for(int i=0; i<arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+        // print all sub-array
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                for(int k=i; k<=j; k++){
+                    System.out.print(arr[k]);
+                }
+                System.out.println();
+            }
+        }
+        in.close();
+    }
 }
