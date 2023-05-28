34d26f31da30c4214247109221998a7fd403dc67
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-28 21:11:21 +0530

    selection sort

--- 08. Basic Sorting algorithms/selectionSort.java
+++ 08. Basic Sorting algorithms/selectionSort.java
@@ -1,13 +1,28 @@
 import java.util.*;
 
 public class selectionSort {
+
+    public static void selectionSortOnArray(int arr[]){
+        // selection sort
+        for(int pos = 0; pos<arr.length-1; pos++){
+            int minPosition = pos;
+            for(int j=pos+1; j<arr.length; j++){
+                if(arr[j] < arr[minPosition]){
+                    minPosition = j;
+                }
+            }
+            int temp = arr[pos];
+            arr[pos] = arr[minPosition];
+            arr[minPosition] = temp;
+        }
+    }
     public static void main(String args[]){
         // selection sort
         Scanner in = new Scanner(System.in);
         // take size as input
         int size = in.nextInt();
         // Declaring array
-        int arr[] = new arr[size];
+        int arr[] = new int[size];
         // enter the values
         for(int i=0; i<arr.length; i++){
             arr[i] = in.nextInt();
