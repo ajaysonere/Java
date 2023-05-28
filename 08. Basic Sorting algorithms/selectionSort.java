5164d1dcd81c755736a104fc6b0b0ab9529bf27f
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-28 21:02:09 +0530

    selection function  sort

--- 08. Basic Sorting algorithms/selectionSort.java
+++ 08. Basic Sorting algorithms/selectionSort.java
@@ -1,3 +1,25 @@
+import java.util.*;
+
 public class selectionSort {
-    
+    public static void main(String args[]){
+        // selection sort
+        Scanner in = new Scanner(System.in);
+        // take size as input
+        int size = in.nextInt();
+        // Declaring array
+        int arr[] = new arr[size];
+        // enter the values
+        for(int i=0; i<arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+
+        // calling the selection sort
+        selectionSortOnArray(arr);
+        // print the element of the array
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        in.close();
+    }
 }
