370f3245fc697532df711bae39ce48c640ebb6d9
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-03 11:19:06 +0530

    is number is zero , positive or negative

--- 03. Conditional statement/isNumberPositive.java
+++ 03. Conditional statement/isNumberPositive.java
@@ -1,3 +1,12 @@
 public class isNumberPositive {
-    
+    public static void main(String args[]){
+        int number = Integer.parseInt(args[0]);
+        if(number > 0){
+            System.out.println("Number is Positive " + number);
+        }else if(number < 0){
+            System.out.println("Number is Negative " + number);
+        }else{
+            System.out.println("Number is Zero ");
+        }
+    }
 }
