bc09db3b064de531c8fc803a74be712e1003d4fb
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-03 11:24:21 +0530

    is last digit same

--- 03. Conditional statement/isLastDigitSame.java
+++ 03. Conditional statement/isLastDigitSame.java
@@ -1,3 +1,12 @@
 public class isLastDigitSame {
-    
+    public static void main(String args[]){
+        int firstNumber = Integer.parseInt(args[0]);
+        int secondNumber = Integer.parseInt(args[1]);
+
+        if((firstNumber%10) == (secondNumber%10)){
+            System.out.println("Last Digit is same ");
+        }else{
+            System.out.println("Last Digit is not same ");
+        }
+    }
 }
