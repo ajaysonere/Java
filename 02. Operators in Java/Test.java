584c3ed864f0acad99633c5a1a5bfdfe671eb066
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-03 11:13:38 +0530

    test

--- 02. Operators in Java/Test.java
+++ 02. Operators in Java/Test.java
@@ -1,3 +1,20 @@
 public class Test {
-    
+    public static void main(String args[]){
+        // int len = args.length;
+        // System.out.println("Lenght is " + len);
+        // float f = 1.1;
+        // boolean b = 1;
+        // double d = 1.2D;
+        // System.out.println(10+017+0X3A);
+        // System.out.println(017);
+        // System.out.println(0X3A);
+        // int i_val = 10;
+        // int j_val = 20;
+        // boolean chk ;
+        // chk = i_val < j_val;
+        // System.out.print(chk);
+        for( ; ;){
+            System.out.println("Helo");
+        }
+    }
 }
