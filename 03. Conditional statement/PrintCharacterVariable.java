51107406a934f044dda4a825d6a644964989387d
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-04 10:51:08 +0530

    print character variable

--- 03. Conditional statement/PrintCharacterVariable.java
+++ 03. Conditional statement/PrintCharacterVariable.java
@@ -1,3 +1,15 @@
+
+
 public class PrintCharacterVariable {
-    
+    public static void main(String args[]){
+        char value = (args[0]).charAt(0);
+
+        if(Character.isDigit(value)){
+            System.out.println("Digit");
+        }else if(Character.isLetter(value)){
+            System.out.print("Alphabet");
+        }else{
+            System.out.println("Special Character");
+        }
+    }
 }
