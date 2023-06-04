1b14c0447dfa0f910f1a80ce3d56a92771f9acc9
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-04 13:12:57 +0530

    sum and

--- 07.  Array/SumAndAverage.java
+++ 07.  Array/SumAndAverage.java
@@ -1,3 +1,35 @@
+import java.util.*;
 public class SumAndAverage {
-    
+
+    public static int sumOfArray(int arr[]){
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    static int averageOfArray(int arr[] , int sumOfArrayElement){
+        int average = 0;
+        int numberOfElementInArray = arr.length;
+        average = (sumOfArrayElement/numberOfElementInArray);
+        return average;
+    }
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        // size of the array
+        int size = in.nextInt();
+        // initializing the array
+        int arr[] = new int[size];
+        // 
+        for(int i=0; i<arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+        
+        int sumOfArrayElement = sumOfArray(arr);
+        int averageOfArrayElement = averageOfArray(arr , sumOfArrayElement);
+        System.out.println("Sum of the array element " + sumOfArrayElement);
+        System.out.println("Average of the array element " + averageOfArrayElement);
+        in.close();
+    }
 }
