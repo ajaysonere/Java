4948404b4223d4d720e3ed59d731a79301e51844
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-04 13:26:49 +0530

    ascii to char

--- 07.  Array/AsciiToChar.java
+++ 07.  Array/AsciiToChar.java
@@ -3,7 +3,21 @@ import java.util.*;
 public class AsciiToChar {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-        
+        int size = in.nextInt();
+        int arr[] = new int[size];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+        // Numbers 
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        // corresponding value
+        System.out.println();
+        for(int i=0; i<arr.length; i++){
+            
+            System.out.print((char)arr[i] + " ");
+        }
         in.close();
     }
 }
