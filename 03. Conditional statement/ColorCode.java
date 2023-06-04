f28624c6e49945ead954d5ed4dac4b1a2df3edfc
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-04 11:04:05 +0530

    Color Code

--- 03. Conditional statement/ColorCode.java
+++ 03. Conditional statement/ColorCode.java
@@ -1,3 +1,21 @@
 public class ColorCode {
-    
+    public static void main(String args[]){
+        char code = (args[0]).charAt(0);
+        switch(code){
+            case 'R' : System.out.println("Red");
+                     break;
+            case 'G' : System.out.println("Green");
+                     break;
+            case 'B' : System.out.println("Blue");
+                     break;
+            case 'O' : System.out.println("Orange");
+                     break;
+            case 'Y' : System.out.println("Yellow");
+                     break;
+            case 'W' : System.out.println("White");
+                     break;
+            default : System.out.println("Invalid Code");
+            
+        }
+    }
 }
