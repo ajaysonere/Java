d3a07c505edbc5dc41af39589c97f1b9388d5521
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-04 12:58:39 +0530

    sum of digit

--- 04. Loops/SumOfDigit.java
+++ 04. Loops/SumOfDigit.java
@@ -1,3 +1,12 @@
 public class SumOfDigit {
-    
+    public static void main(String args[]){
+        int number = Integer.parseInt(args[0]);
+
+        int sum = 0;
+        while(number > 0){
+         sum = sum+number%10;
+         number /= 10;
+        }
+        System.out.println("Sum is :"+ sum);
+    }
 }
