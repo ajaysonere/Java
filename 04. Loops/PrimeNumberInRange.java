e5ebedf4056197269f75f246e7f5a1f0c32d63bd
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-04 11:26:59 +0530

    Prime Number In Range

--- 04. Loops/PrimeNumberInRange.java
+++ 04. Loops/PrimeNumberInRange.java
@@ -1,3 +1,20 @@
 public class PrimeNumberInRange {
-    
+    public static boolean isPrimeNumber(int number){
+        for(int i=2; i<=Math.sqrt(number); i++){
+            if(number%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        int firstNumber = Integer.parseInt(args[0]);
+        int secondNumber = Integer.parseInt(args[1]);
+        
+        for(int i=firstNumber; i<secondNumber; i++){
+            if(isPrimeNumber(i)){
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
