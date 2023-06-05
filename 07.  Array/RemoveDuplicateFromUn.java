cacdce8a0079579a165a3fd94ff51f0dda1eaf6b
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-05 18:56:15 +0530

    Sum of element with condition

--- 07.  Array/RemoveDuplicateFromUn.java
+++ 07.  Array/RemoveDuplicateFromUn.java
@@ -3,7 +3,23 @@ import java.util.*;
 public class RemoveDuplicateFromUn {
     public static void main(String args[]){
         Scanner in  = new Scanner(System.in);
+        // size of array
+        int size = in.nextInt();
+        //declare an array
+
+        int arr[] = new int[size];
+        // provided input to array
+        for(int i=0; i<arr.length; i++){
+            arr[i] = in.nextInt();
+        }
         
-        in.close();
-    }
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] == 0) continue;
+                if(arr[i] == arr[j]){
+                    arr[j] = 0;
+                }
+            }
+        }
+    } 
 }
