70e1f48ccddafbb82ee11c62d1295769d3e7a62c
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-05 19:38:19 +0530

    Sum of Element In Condition

--- 07.  Array/RemoveDuplicateFromUn.java
+++ 07.  Array/RemoveDuplicateFromUn.java
@@ -21,5 +21,6 @@ public class RemoveDuplicateFromUn {
                 }
             }
         }
+        in.close();
     } 
 }
--- 07.  Array/SumOfElementWithCondition.java
+++ 07.  Array/SumOfElementWithCondition.java
@@ -1,3 +1,38 @@
+import java.util.*;
+
 public class SumOfElementWithCondition {
-    
+
+    public static int sumOfElementInCondition(int arr[]){
+        int sum = 0;
+        int tempSum = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 6){
+                for(; i<arr.length; i++){
+                    if(arr[i] == 7){
+                        tempSum = 0;
+                        break;
+                    }else{
+                        tempSum += arr[i];
+                    }
+                }
+            }else{
+                sum += arr[i];
+            }
+        }
+
+        return sum+tempSum;
+    }
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        int arr[] = new int[size];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+
+        int sumOfElement = sumOfElementInCondition(arr);
+        System.out.println("sum of the element is " + sumOfElement);
+        
+        in.close();
+    }
 }
