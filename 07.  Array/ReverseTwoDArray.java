98b3bf6b140ebec9ee3d702ce01f734abf630f40
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-07 11:25:08 +0530

    Revrse two d

--- 07.  Array/ReverseTwoDArray.java
+++ 07.  Array/ReverseTwoDArray.java
@@ -1,3 +1,25 @@
 public class ReverseTwoDArray {
-    
+    public static void main(String args[]){
+        int arr[][] = new int[2][2];
+        arr[0][0] = Integer.parseInt(args[0]);
+        arr[0][2] = Integer.parseInt(args[1]);
+        arr[1][0] = Integer.parseInt(args[2]);
+        arr[1][1] = Integer.parseInt(args[3]);
+
+        int row = arr.length;
+        int col = arr[0].length;
+        int newArray [][] = new int[row][col];
+        for(int i=row-1; i>=0; i--){
+            for(int j=col-1; j>=0; j--){
+                newArray[row-i-1][col-j-1] = arr[i][j];
+            }
+        }
+
+        for(int i=0; i<newArray.length; i++){
+            for(int j=0; j<newArray[i].length; j++){
+                System.out.print(newArray[i][j] + " ");
+            }
+        }
+        
+    }
 }
