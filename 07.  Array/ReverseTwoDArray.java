a19e7e10af3950e4f317a377f2620b6bd3580448
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-07 15:52:09 +0530

    reversed array

--- 07.  Array/ReverseTwoDArray.java
+++ 07.  Array/ReverseTwoDArray.java
@@ -2,7 +2,7 @@ public class ReverseTwoDArray {
     public static void main(String args[]){
         int arr[][] = new int[2][2];
         arr[0][0] = Integer.parseInt(args[0]);
-        arr[0][2] = Integer.parseInt(args[1]);
+        arr[0][1] = Integer.parseInt(args[1]);
         arr[1][0] = Integer.parseInt(args[2]);
         arr[1][1] = Integer.parseInt(args[3]);
 
@@ -15,10 +15,21 @@ public class ReverseTwoDArray {
             }
         }
 
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println("Reversed Array");
+
         for(int i=0; i<newArray.length; i++){
             for(int j=0; j<newArray[i].length; j++){
                 System.out.print(newArray[i][j] + " ");
             }
+            System.out.println();
         }
         
     }
