00e82618aa9cb8e420324783d4525edb378ab7cd
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-07 19:13:39 +0530

    largest element in 3*3 array

--- 07.  Array/SearchInMatrix.java
+++ 07.  Array/SearchInMatrix.java
@@ -1,3 +1,24 @@
 public class SearchInMatrix {
-    
+    public static void main(String args[]){
+        int arr[][] = new int[3][3];
+        int index = 0;
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                arr[i][j] = Integer.parseInt(args[index++]);
+            }
+        }
+
+       
+        int largest = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                if(arr[i][j] >largest){
+                    largest = arr[i][j];
+                }
+            }
+        }
+
+        System.out.println("Largest Element is " + largest);
+
+    }
 }
