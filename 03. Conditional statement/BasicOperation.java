886050b7ed6efeaab856a8af64b539098610fec0
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-08 21:48:22 +0530

    even is implemented

--- 03. Conditional statement/BasicOperation.java
+++ 03. Conditional statement/BasicOperation.java
@@ -1,3 +1,13 @@
 public class BasicOperation {
-    
+
+    public static boolean isOddNum(int n){
+        if(n%2 == 0)
+          return false;
+        return true;
+    }
+    public static void main(String args[]){
+        boolean isOddNumber = isOddNum(10);
+        boolean isEvenNumber = isEvenNum(5);
+
+    }
 }
