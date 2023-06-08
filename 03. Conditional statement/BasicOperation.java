808666fdc67ec1e53df441b7b4ab2dad8c3ace39
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-08 22:11:41 +0530

    sum of two last digit

--- 03. Conditional statement/BasicOperation.java
+++ 03. Conditional statement/BasicOperation.java
@@ -24,15 +24,25 @@ public class BasicOperation {
         n = n/10;
         return n%10;
     }
+
+    public static int getSumOfLastDigit(int a , int b){
+        a = Math.abs(a);
+        b = Math.abs(b);
+       return ((a%10)+(b%10));
+    }
     public static void main(String args[]){
         boolean isOddNumber = isOddNum(10);
         boolean isEvenNumber = isEvenNum(5);
         int lastDigit = getLastDigit(-4523);
         int secondLastDigit = getSecondLastDigit(-4);
+        int sumOfTwoLastDigit = getSumOfLastDigit(142 , -542);
+
         System.out.println(isOddNumber);
         System.out.println(isEvenNumber);
         System.out.println(lastDigit);
         System.out.println(secondLastDigit);
+        System.out.println(sumOfTwoLastDigit);
+        
         
 
     }
