83d8325f2755f111b9ae38a8ddfc8cd768c443b9
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-09 11:03:44 +0530

    completed exect multiple

--- 03. Conditional statement/ExectMultiple.java
+++ 03. Conditional statement/ExectMultiple.java
@@ -1,3 +1,11 @@
 public class ExectMultiple {
-    
+    public static void main(String args[]){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        if(a%b == 0){
+           System.out.println("Exect Multiple ");
+        }else{
+            System.out.print("Not Exect Multiple");
+        }
+    }
 }
