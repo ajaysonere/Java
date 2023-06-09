88df309e99128a70a9f2fead81dbed27ac85f3b7
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-09 23:48:28 +0530

    static class implemented

--- 09. Object Oriented/BasicClass.java
+++ 09. Object Oriented/BasicClass.java
@@ -1,3 +1,15 @@
+class Temp{
+    int x;
+    int y;
+
+    void addition(int x, int y){
+        this.x = x;
+        this.y = y;
+        System.out.println(x+y);
+    }
+}
+
 public class BasicClass {
-    
+    Temp a = new Temp();
+    a.addition(10,20);
 }
