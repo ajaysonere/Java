18c1fe0be9d187de5fd3905710e332a1efbba747
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-10 22:16:39 +0530

    how many even and odd

--- 03. Conditional statement/HowManyEvenAndOdd.java
+++ 03. Conditional statement/HowManyEvenAndOdd.java
@@ -1,3 +1,29 @@
 public class HowManyEvenAndOdd {
-    
+    public static void main(String args[]){
+        int a = 10 ,b = 30 , c = 45 ,d = 65 , e = 25;
+        int even , odd;
+        even = odd = 0;
+        if(a%2==0)
+            even++;
+        else
+            odd++;
+        if(b%2==0)
+            even++;
+        else
+            odd++;
+        if(c%2==0)
+            even++;
+        else
+            odd++;
+
+        if(d%2==0)
+            even++;
+        else
+            odd++;
+        if(e%2==0)
+            even++;
+        else
+            odd++;
+        System.out.println("Even Number :"+even + "\nOdd Number :"+odd);
+    }
 }
