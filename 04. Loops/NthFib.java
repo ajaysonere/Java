db94d08f8ff5b6a4e1d3593566e3f2d97eebe6af
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-12 23:30:05 +0530

    n th fibonacci number

--- 04. Loops/NthFib.java
+++ 04. Loops/NthFib.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class NthFib {
-    
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int NthFibNumber = in.nextInt();
+        int a = 0;
+        int b = 0;
+        int c = 1;
+        int count = 2;
+        while(count < NthFibNumber){
+            a = b;
+            b = c;
+            c = a+b;
+            count++;
+        }
+        System.out.println("N th Fibonacci Number : " + c);
+    }
 }
