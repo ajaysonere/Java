d8e623932557f5038f8febd112d0fa12caa2aa1f
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-13 18:36:35 +0530

    Volume of the class

--- 09. Object Oriented/VolumeOfClass.java
+++ 09. Object Oriented/VolumeOfClass.java
@@ -1,3 +1,20 @@
+class Box{
+    int width;
+    int depth;
+    int height;
+
+    Box(int width , int depth , int height){
+        this.width = width;
+        this.depth = depth;
+        this.height = height;
+    }
+    int volumeOfBox(){
+       return width*depth*height;
+    }
+}
 public class VolumeOfClass {
-    
+    public static void main(String args[]){
+        Box b = new Box(10,20,30);
+        System.out.println("Volumn of the object : " + b.volumeOfBox());
+    }
 }
