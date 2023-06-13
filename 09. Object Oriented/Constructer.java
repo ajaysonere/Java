8697dca64f8513f79809d7bd2bc1c70a4075c398
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-13 17:11:02 +0530

    Constructer

--- 09. Object Oriented/Constructer.java
+++ 09. Object Oriented/Constructer.java
@@ -1,3 +1,21 @@
+class Sample{
+    int id;
+    Sample(){
+        id = 101;
+        System.out.println("Default constructer " + id);
+    }
+    Sample(int id){
+       this.id = id;
+       System.out.println("Parameterized Constructer " +id);
+    }
+    void display(){
+        System.out.println("Instance id "+ id);
+    }
+}
+
 public class Constructer {
-    
+    public static void main(String args[]){
+        Sample s1  = new Sample(10);
+        s1.display();
+    }
 }
