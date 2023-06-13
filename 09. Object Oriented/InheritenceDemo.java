9a97780ba19f0b2ee41ded56c99226fc1ce1f3e0
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-13 22:19:04 +0530

    singal inheritence

--- 09. Object Oriented/InheritenceDemo.java
+++ 09. Object Oriented/InheritenceDemo.java
@@ -1,3 +1,31 @@
+class A{
+    int a;
+    void display(){
+        System.out.println("From Class A");
+    }
+}
+
+class B extends A{
+    int b;
+    void display1(){
+        System.out.println("From class B");
+    }
+
+    void sum(){
+        System.out.println("Sum is : " + (a+b));
+    }
+}
 public class InheritenceDemo {
-    
+    public static void main(String args[]){
+         A a1 = new A();
+         B b1 = new B();
+
+        b1.a = 10;
+        b1.b = 20;
+        b1.display();
+        b1.sum();
+
+        a1.display();
+        
+    }
 }
