5fe18582c830046ff3e4550fcb8d3b8e2795651b
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-16 22:22:44 +0530

    input output stream

--- 12. Input Output Stream/InputStream.java
+++ 12. Input Output Stream/InputStream.java
@@ -8,8 +8,8 @@ public class InputStream {
             String text = "My name is Ajay Sonere .";
             Path input = Path.of("D:/Source Code/Java/12. Input Output Stream/input.txt");
             // Files.writeString(input, text);
-            String content = Files.readString(input);
-            System.out.println(content);
+            String content = Files.readString(input); 
+            System.out.println(content + text);
         } catch (Exception e) {
             
         }
