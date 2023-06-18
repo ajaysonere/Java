dabb45baa36b4ee3d6a40b481cb13cff0fb0b708
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-18 14:46:51 +0530

    string compare

--- 14. String/Basic.java
+++ 14. String/Basic.java
@@ -1,3 +1,10 @@
 public class Basic {
-    
+    public static void main(String args[]){
+        String name = "Ajay";
+        String name1 = "Ajay";
+        System.out.println(name == name1);
+        String name2 = new String("Sonere");
+        String name3 = new String("Sonere");
+        System.out.println(name2 == name3);
+    }
 }
