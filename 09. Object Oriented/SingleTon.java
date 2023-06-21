b05b4edf32101237926ad15b3e1930879a63797e
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-21 23:38:49 +0530

    single tone

--- 09. Object Oriented/SingleTon.java
+++ 09. Object Oriented/SingleTon.java
@@ -1,3 +1,21 @@
 public class SingleTon {
-    
+
+    private SingleTon(){
+
+    }
+
+    private static SingleTon instance;
+
+    public static SingleTon getInstance(){
+        if(instance == null){
+            instance = new SingleTon();
+        }
+        return instance;
+    }
+
+    public static void main(String args[]){
+        SingleTon obj = SingleTon.getInstance();
+        
+    }
 }
+
