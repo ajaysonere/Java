f13958a57efa3c583b3a9957c7bf2cd8c31cf1d3
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-23 21:47:19 +0530

    Thread in java

--- 13. Interface/Thread_exp.java
+++ 13. Interface/Thread_exp.java
@@ -1,3 +1,21 @@
+class MultiThreadDemo implements Runnable{
+    public void run(){
+        try {
+            System.out.println("Thread is runnig ");
+        } catch (Exception e) {
+            System.out.println("Exception is caught");
+        }
+    }
+
+}
+
 public class Thread_exp {
-    
+    public static void main(String args[]){
+        for(int i=0; i<5; i++){
+            Thread obj = new Thread(new MultiThreadDemo());
+            Thread obj1 = new Thread(new MultiThreadDemo());
+            obj.start();
+            obj1.start();
+        }
+    }
 }
