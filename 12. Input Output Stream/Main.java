4b63bd2f3036ec63bec049a50f833a7c8f926acb
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-24 19:47:43 +0530

    write stream complete

--- 12. Input Output Stream/Main.java
+++ 12. Input Output Stream/Main.java
@@ -1,8 +1,17 @@
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class Main {
     public static void main(String[] args) {
-        BufferedWriter write = new BufferedWriter();
-        
+        try{
+            BufferedWriter wr = new BufferedWriter(new FileWriter("D:\\Source Code\\Java\\12. Input Output Stream\\input.txt"));
+            wr.write("Hi , 👋 My name is Ajay\n");
+            wr.write("Nice to meet you\n");
+            wr.write("Thank you \n");
+            wr.close();
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
