463a075574beceea1189837f0d65049821f5e34f
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-24 15:22:57 +0530

    Abstract class

--- 09. Object Oriented/polymorphism/AbstractClass.java
+++ 09. Object Oriented/polymorphism/AbstractClass.java
@@ -1,3 +1,24 @@
-public class AbstractClass {
-    
+
+abstract class Basic {
+    public abstract void run();
+    public abstract void fun();
 }
+
+class ChildAbs extends Basic{
+    public void run(){
+        System.out.println("Abstract Method run");
+     }
+    public void fun(){
+        System.out.println("Abstract Method fun");
+    }
+}
+
+public class AbstractClass{
+    public static void main(String args[]){
+        ChildAbs object = new ChildAbs();
+        object.fun();
+        object.run();
+    }
+}
+
+
