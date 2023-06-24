99a2ffc1fc4f626ce6bbe5ac5b1ef63f4d0c44e3
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-24 19:40:41 +0530

    Write Stream in java

--- 16. Exeption in java/Main.java
+++ 16. Exeption in java/Main.java
@@ -1,3 +1,13 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 1;
+        // int b = 0;
+        try{
+           int c = a/b;
+           System.out.println(c);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
