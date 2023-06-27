d4327db2d84d1ed26333c6f2fa12c137d05afdf9
Ajay Sonere <ajaysonere786@gmail.com>
2023-06-27 16:27:50 +0530

    jdbc

--- 17. JDBC/JDBCExample.java
+++ 17. JDBC/JDBCExample.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.*;
+
 
 public class JDBCExample {
     public static void main(String[] args) {
